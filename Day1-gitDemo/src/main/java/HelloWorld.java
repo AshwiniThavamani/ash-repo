db932d15a90e8648d545d54733e4b1c6cce38f1d
athavama <athavama@DIN73002971.corp.capgemini.com>
2018-08-06 10:34:26 +0530

    My first Commit

---
+++ Day1-gitDemo/src/main/java/HelloWorld.java
@@ -0,0 +1,16 @@
+
+public class HelloWorld {
+
+	public static void Mainclass(String args[]) 
+	{
+		System.out.println("hello");
+		greet();
+	}
+
+	private static void greet() {
+		// TODO Auto-generated method stub
+		System.out.println("in method");
+	}
+	
+	
+}
